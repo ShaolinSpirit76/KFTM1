f77b87dd57198cce259dc17930afcdce32bd704f
ShaolinSpirit76 <paul.compere76@gmail.com>
2019-07-23 15:19:25 +0200

    Create ok !

--- controller/inscription.php
+++ controller/inscription.php
@@ -1,13 +1,38 @@
 <?php
 
+//il faut tjrs appeler la base de donnée dans le controller
+require '../../model/DataBase.php'; 
+require '../../model/User.php';
+
 // On crée un tableau error qui s'auto-incrémentera avec la valeur de l'erreur que nous lui assignerons au cas par cas, si la regex n'est pas franchie. Chaque cellule remplie comptera pour 1.
 $error = [];
 $users = new User();
+
 // On teste les regex si le formulaire est rempli
-if (count($_POST) > 0)
- {
+if (count($_POST) > 0):
+
+    // Déclaration de variables qui prennent les valeurs des $_POST respectives
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $birthDate = $_POST['birthDate'];
+    $picture = $_POST['picture'];
+    $mail = $_POST['mail'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $status = $_POST['status'];
+    $studentCourse = $_POST['studentCourse'];
+    $teacherCourse = $_POST['teacherCourse'];
+    $groupAge = $_POST['groupAge'];
+    $studentYear = $_POST['studentYear'];
+    $childBelt = $_POST['childBelt'];
+    $studentBelt = $_POST['studentBelt'];
+    $teacherRank = $_POST['teacherRank'];
+    $presentation = $_POST['presentation'];
+    // $verification = $_POST['verification'];
+
     
-    $_POST= array_map('strip_tags', $_POST); // Ligne à mettre absolument pour la sécurité : interdit l'injection de script (balises non autorisées)
+    $_POST = array_map('strip_tags', $_POST); // Ligne à mettre absolument pour la sécurité : interdit l'injection de script (balises non autorisées)
 
     if ( !preg_match ($regexName, $_POST['lastName'] ) ):
         $error['errorLastName'] = 'Votre nom de famille est incorrect.';
@@ -21,30 +46,39 @@ if (count($_POST) > 0)
             $users->firstName = $firstName;
      endif;
 
-    if ( !preg_match ($regexDate, date('d/m/Y',strtotime($_POST['birthDate']) ) ))
-    {
+    if ( !preg_match ($regexDate, date('d/m/Y',strtotime($_POST['birthDate']) ) )):
         $error['errorBirthDate'] = 'Votre date de naissance est incorrecte.';
-    } 
-    if ( !preg_match ($regexMail, $_POST['mail'] ) )
-    {
+        elseif (preg_match ($regexDate, date('d/m/Y',strtotime($_POST['birthDate']) ))):
+            $users->birthDate = $birthDate;
+    endif; 
+
+    if ( !preg_match ($regexMail, $_POST['mail'] ) ):
         $error['errorMail'] = 'Votre adresse mail est incorrect.';
-    } 
-    if (!preg_match ($regexPhone, $_POST['phoneNumber']))
-    {
+        elseif (preg_match ($regexMail, $_POST['mail'] )):
+            $users->mail = $mail;
+    endif;
+
+    if (!preg_match ($regexPhone, $_POST['phoneNumber'])):
         $error['errorPhone'] = 'Votre numéro de téléphone est incorrect.';
-    } 
-    if (!preg_match ($regexLogin, $_POST['login']))
-    {
+        elseif(preg_match ($regexPhone, $_POST['phoneNumber'])):
+            $users->phoneNumber = $phoneNumber;
+    endif; 
+
+    if (!preg_match ($regexLogin, $_POST['login'])):
         $error['errorLogin'] = 'Votre login est non conforme.';
-    }
-    if (!preg_match ($regexPassword, $_POST['password']))
-    {
+        elseif(preg_match ($regexLogin, $_POST['login'])):
+            $users->login = $login;
+    endif;
+
+    if (!preg_match ($regexPassword, $_POST['password'])):
         $error['errorPassword'] = 'Votre mot de passe est incorrect.';
-    }
-    if (!preg_match ($regexPassword, $_POST['confirmPassword']))
-    {
+        elseif(preg_match ($regexPassword, $_POST['password'])):
+            $users->password = $password;
+    endif;
+
+    if (!preg_match ($regexPassword, $_POST['confirmPassword'])):
         $error['errorConfirmPassword'] = 'La confirmation de votre mot de passe est incorrecte.';
-    }
+    endif;
 
 
     // modal error s'il y a une erreur
@@ -80,46 +114,11 @@ setTimeout(function(){
 </script>
 <?php endif; 
 
-
-}
-
-
-
-
-//il faut tjrs appeler la base de donnée dans le controller
-require '../../model/DataBase.php'; 
-require '../../model/User.php';
     
-if(count($_POST) > 0){
-    $lastName = $_POST['lastName'];
-    $firstName = $_POST['firstName'];
-    $birthDate = $_POST['birthDate'];
-    $picture = $_POST['picture'];
-    $mail = $_POST['mail'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-    $status = $_POST['status'];
-    $studentCourse = $_POST['studentCourse'];
-    $teacherCourse = $_POST['teacherCourse'];
-    $groupAge = $_POST['groupAge'];
-    $studentYear = $_POST['studentYear'];
-    $childBelt = $_POST['childBelt'];
-    $studentBelt = $_POST['studentBelt'];
-    $teacherRank = $_POST['teacherRank'];
-    $presentation = $_POST['presentation'];
-    // $verification = $_POST['verification'];
         
-    //objet qui contient les attributs et les méthodes de la class User
+//objet qui contient les attributs et les méthodes de la class User
     
-    
-   
-    $users->birthDate = $birthDate;
-    $users->picture = $picture;
-    $users->mail = $mail;
-    $users->phoneNumber = $phoneNumber;
-    $users->login = $login;
-    $users->password = $password;
+    $users->picture = $picture;    
     $users->status = $status;
     $users->studentCourse = $studentCourse;
     $users->teacherCourse = $teacherCourse;
@@ -130,9 +129,8 @@ if(count($_POST) > 0){
     $users->teacherRank = $teacherRank;
     $users->presentation = $presentation;
     // $users->verification = $verification;
+    $users->addUser();
 
-    if($users->addUser()){
-       
 
-    }
-}
+
+endif;
